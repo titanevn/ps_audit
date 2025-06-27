5ef067ee09e03c4e650f4cac5b8cc0a90c5f2014
titanevn <100345486+titanevn@users.noreply.github.com>
2025-06-27 18:54:04 +0700

    Update win-assessment.ps1

--- win-assessment.ps1
+++ win-assessment.ps1
@@ -7,7 +7,7 @@ Get-MpComputerStatus | select RealTimeProtectionEnabled
 
 #Get Firewall status
 Get-NetFirewallProfile | Format-Table Name, Enabled
-
+Get-NetFirewallRule | select DisplayName, Enabled, Description
 
 #Get Running process
 
